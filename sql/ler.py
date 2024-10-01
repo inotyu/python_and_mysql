6359a8f4f090819ae7d279d7786440493381cc11
Gean Carlos <146786236+inotyu@users.noreply.github.com>
2024-10-01 15:08:04 -0300

    Delete sql directory

--- sql/ler.py
+++
@@ -1,9 +0,0 @@
-def ler(cursor):
-    comando = "SELECT * FROM PESSOA"
-    cursor.execute(comando)
-    resultado = cursor.fetchall()
-    
-    print(f'- {"ID".ljust(3)} -- {"Nome".ljust(15)} -- {"Preço".ljust(5)}')
-    for i,j in enumerate(resultado,start=1):
-        a = j
-        print(f'{i} │ {str(j[0]).ljust(3)} │ {j[1].ljust(15)} │ {str(j[2]).ljust(5)}')
